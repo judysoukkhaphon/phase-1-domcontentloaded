89aa6eb0007ebcb54fadead019cdb8d98aaea7a7
Judy Soukkhaphon <judysoukkhaphon@gmail.com>
2023-01-17 17:21:47 -0500

    simple DOMContentLoaded event to change text

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', () => {
+    let element = document.getElementById('text');
+    element.textContent = 'This is really cool!';
+})
+
+
+// Also works.
+/*
+document.addEventListener('DOMContentLoaded', () => {
+    let element = document.querySelector('p');
+    element.textContent = 'This is really cool!';
+})
+*/
